eb97bdea2a5ef2664c39a9724134a42076b2c4c7
juaccardenasgom <juaccardenasgom@unal.edu.co>
2018-05-09 22:12:18 -0400

    Automatic commit. Done before go to queltrics

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
-## webapp_phase1
+# Projectsito ![CI status](https://img.shields.io/badge/build-passing-brightgreen.svg)
 
-undefined
+Projecsito is a project made for 'Purdue UNAL IronHacks' contest, Spring 2018. 
+
+## Keywords
+
+`Students` `Affordability` `Creativity` `Design` `Mashup`
+
+## Description of the datasets and function design
+
+* At the moment, just dataset from `NY Districts geoshapes` and `Neighborhood Names GIS`.
+* Primary dataset from `data.gov` hasn't been fully implemented, although it's mentioned in the code.
+
+## Brief Description
+### Introduction
+This is projectsito. The solution for the task proposed in the contest. It's meant to meet the minimum requirements of the task while keeping creativity and imagination. It's important to Projectsito to be user-friendly and to help people to find a home to live.
+### Meeting the requirements
+
+* Map Visualization: Districts sketch have been displayed on the map.
+* Chart Visualization: No chart has been implemented yet.
+* Interaction Form: For interaction, there's only GUI.
+
+## Build Case
+In order to use the project you simply should download every file and folder from the project and execute `index.html` with a valid web browser.
+
+## Test Case 
+Projectsito has only been tested on Chrome browser.
+
+## Additional Info
+I currently have an issue with the `Neighborhood Names GIS` dataset. And I'm not completely sure if I'm meeting `image-rendering requirements` and guidelines.
--- index.js
+++ index.js
@@ -1,6 +1,76 @@
 
-(function () {
-    document.getElementById("btn").addEventListener("click", function () {
-        alert("Hi! :)");
-    });
-})();
+var map;
+
+var rawLocations;
+var locations = [];
+
+const URLGeoJSON = "https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nycd/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson"
+const URLDistsInfo = "https://data.cityofnewyork.us/api/views/xyye-rtrs/rows.json?accessType=DOWNLOAD";
+
+function initMap() {
+        var customCenter = {lat:40.702649, lng:-73.993746}//Aestethic location
+        //map
+        map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 10.9, //Map's zoom
+          center: customCenter,//
+          mapTypeId: 'terrain'
+        });
+        //marker
+        var nyu_coordinates = {lat:40.7290549,lng:-73.9965233};
+        var ny_marker = new google.maps.Marker({//New marker instance
+          position: nyu_coordinates,
+          map: map
+        });
+
+        //Districts
+        map.data.loadGeoJson(URLGeoJSON);//Show me the districts.
+
+        //Districs' display style
+        map.data.setStyle(function(feature) {//Copy-Paste of Example Google's "Data Layer: Dynamic Styling"
+          var color = '#2A7F62';
+          if (feature.getProperty('isColorful')) {
+            color = feature.getProperty('color');
+          }
+          return /** @type {google.maps.Data.StyleOptions} */({
+            fillColor: color,
+            fillOpacity: 0.01,
+            strokeColor: color,
+            strokeWeight: 2
+          });
+        });
+
+        // When the user clicks, set 'isColorful', changing the color of the letters.
+        map.data.addListener('click', function(event) {
+          event.feature.setProperty('isColorful', true);
+        });
+
+        // When the user hovers, tempt them to click by outlining the letters.
+        // Call revertStyle() to remove all overrides. This will use the style rules
+        // defined in the function passed to setStyle()
+        map.data.addListener('mouseover', function(event) {
+          map.data.revertStyle();
+          map.data.overrideStyle(event.feature, {strokeWeight: 8});
+        });
+
+        map.data.addListener('mouseout', function(event) {
+          map.data.revertStyle();
+        });
+
+        //For future markers
+        getDataFromURL(URLDistsInfo);
+      }
+
+      function getDataFromURL(URL){
+        var data = $.get(URL, function(array){
+      		console.log(URL)
+      	})
+      		.done( function(){//Make me a new Locations Array
+      			//Success
+      			//console.log(data);
+
+            for (var i = 0; i < data.responseJSON.data.length; i++) {//making through raw latlng rawLocations and making positions
+              rawLocations = data.responseJSON.data[i][9];
+              locations[i] = {lat: parseFloat(rawLocations.toString().substring(7,rawLocations.toString().lastIndexOf(" "))), lng:parseFloat(rawLocations.toString().substring(rawLocations.toString().lastIndexOf(" ")))};
+            }
+        });
+      }
